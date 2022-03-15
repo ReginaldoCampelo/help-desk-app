c7a16883b8b3fb6d21f7287f0d156c21fc18140f
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-15 16:45:50 -0300

    adding login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     HomeComponent,
     NavbarComponent,
     HeaderComponent,
-    TecnicoListComponent
+    TecnicoListComponent,
+    LoginComponent
   ],
   imports: [
     CoreModule,
