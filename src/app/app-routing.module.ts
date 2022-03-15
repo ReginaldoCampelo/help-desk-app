92eb4d66e2d95acb9762edb7aaaf2e42ed57ca0f
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-15 16:46:57 -0300

    adding route to login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  
   { path: '', component: NavbarComponent, children: [
     { path: 'home', component: HomeComponent },
     { path: 'tecnicos', component: TecnicoListComponent }
