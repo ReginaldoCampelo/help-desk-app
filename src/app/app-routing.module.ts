97a68314dcb537cd623d53e75fd2c89c7fd62aa1
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-15 16:13:12 -0300

    adding route to tecnicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   { path: '', component: NavbarComponent, children: [
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'tecnicos', component: TecnicoListComponent }
   ] }
 ];
 
