da105ff607e92dd71371bd214d6bd358cb0c8a89
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-15 21:44:49 -0300

    completing tecnico-list component && tecnico-list template

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'hdk-tecnico-list',
@@ -10,41 +11,30 @@ import { Tecnico } from 'src/app/models/tecnico';
 })
 export class TecnicoListComponent implements OnInit {
 
-  ELEMENT_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Reginaldo Campelo',
-      cpf: '627.627.470-40',
-      email: 'reginaldo@mail.com',
-      senha: '1234',
-      perfis: ['0'],
-      dataCriacao: '15/03/2022'
-    },
-    {
-      id: 2,
-      nome: 'Natalia Damasceno',
-      cpf: '559.839.330-19',
-      email: 'nataliapereirausa@gmail.com',
-      senha: '1234',
-      perfis: ['0'],
-      dataCriacao: '15/03/2022'
-    }
-  ]
+  ELEMENT_DATA: Tecnico[] = []
 
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  constructor(private service: TecnicoService) { }
+
+  ngOnInit(): void {
+    this.findAll();
   }
 
+  findAll() {
+    this.service.findAll().subscribe(response => {
+      this.ELEMENT_DATA = response
+      this.dataSource = new MatTableDataSource<Tecnico>(response);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+  }
 }
 
