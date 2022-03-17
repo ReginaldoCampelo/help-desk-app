b7d8dc278ca0d0d24e646f4aa60c19f387dcdb01
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-17 18:46:48 -0300

    improving chamado-read template

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
+
 
 @Component({
   selector: 'hdk-chamado-read',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoReadComponent implements OnInit {
 
-  constructor() { }
+  chamado: Chamado = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    tecnico: '',
+    cliente: '',
+    nomeCliente: '',
+    nomeTecnico: ''
+  }
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private toastr: ToastrService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(response => {
+      this.chamado = response;
+    }, ex => {
+      this.toastr.error(ex.error.error);
+    })
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0') {
+      return 'Aberto'
+    } else if (status == '1') {
+      return 'Em andamento'
+    } else {
+      return 'Encerrado'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0') {
+      return 'Baixa'
+    } else if (prioridade == '1') {
+      return 'Média'
+    } else {
+      return 'Alta'
+    }
   }
 
 }
