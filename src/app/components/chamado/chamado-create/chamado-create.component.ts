9d691f2b91b7b887fc60b1189711af49d081d192
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-17 13:46:17 -0300

    adding validations form for chamado-create

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'hdk-chamado-create',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  titulo: FormControl = new FormControl(null, [Validators.required, Validators.minLength(3)])
+  descricao: FormControl = new FormControl(null, [Validators.required, Validators.minLength(8)])
+  tecnico: FormControl = new FormControl(null, [Validators.required])
+  cliente: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid &&
+      this.tecnico.valid && this.descricao.valid && this.tecnico.valid && this.cliente.valid
   }
 
 }
