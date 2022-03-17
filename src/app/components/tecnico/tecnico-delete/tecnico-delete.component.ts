ada165cb9a95a632644bb6fd56e4f9edceef2b14
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-16 21:30:54 -0300

    implementing delete method for tecnicos

--- src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'hdk-tecnico-delete',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoDeleteComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
   }
 
+  constructor(private service: TecnicoService, private toastr: ToastrService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe( response => {
+      response.perfis = []
+      this.tecnico = response;
+    })
+  }
+  
+  delete(): void {
+    this.service.delete(this.tecnico.id).subscribe(() => {
+      this.toastr.success('Técnico deletado com sucesso', 'Deleted');
+      this.router.navigate(['tecnicos'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toastr.error(element.message);
+        });
+      } else {
+        this.toastr.error(ex.error.message);
+      }
+    })
+  }
 }
--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    //baseUrl : 'https://hml-helpdesk-api.herokuapp.com'
-     baseUrl : 'http://localhost:8080'
+    baseUrl : 'https://hml-helpdesk-api.herokuapp.com'
+    //baseUrl : 'http://localhost:8080'
 }
